bf56292887f257c652b4253b765ebdb11d2c294c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-11-07 22:08:39 +0100

    trimRight solution

---
+++ challenges/medium/trimRight.ts
@@ -0,0 +1,46 @@
+/*
+  4803 - Trim Right
+  -------
+  by Yugang Cao (@Talljack) #medium 
+  
+  ### Question
+  
+  Implement `TrimRight<T>` which takes an exact string type and returns a new string with the whitespace ending removed.
+  
+  For example:
+  
+  ```ts
+  type Trimed = TrimRight<'   Hello World    '> // expected to be '   Hello World'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/4803
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type WhiteSpace = " " | "\n" | "\t";
+
+type TrimRight<StringToTrim extends string> =
+  StringToTrim extends `${infer Head}${WhiteSpace}`
+    ? TrimRight<Head>
+    : StringToTrim;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<TrimRight<"str">, "str">>,
+  Expect<Equal<TrimRight<"str ">, "str">>,
+  Expect<Equal<TrimRight<"str     ">, "str">>,
+  Expect<Equal<TrimRight<"     str     ">, "     str">>,
+  Expect<Equal<TrimRight<"   foo bar  \n\t ">, "   foo bar">>,
+  Expect<Equal<TrimRight<"">, "">>,
+  Expect<Equal<TrimRight<"\n\t ">, "">>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4803/answer
+  > View solutions: https://tsch.js.org/4803/solutions
+  > More Challenges: https://tsch.js.org
+*/
